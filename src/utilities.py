f612e31a8f8079daba2edde7b73c00dfb939c347
Vinicius Mohr <vinicius.mohr1618@gmail.com>
2024-05-05 11:08:22 +0200

    Added additional utilities

--- src/utilities.py
+++ src/utilities.py
@@ -204,3 +204,130 @@ def generate_regular_3_graph(n_nodes, seed):
     G = nx.random_regular_graph(3, n_nodes, seed=seed)
     edges = list(G.edges())
     return G, edges
+
+def cost_function_qaoa(results, edges):
+ """
+ This function calculates the cost of a max cut solution.
+ Parameters
+ ----------
+ results : list
+     A list of results from a quantum algorithm.
+ edges : list
+     A list of edges in the graph.
+ Returns
+ -------
+ cost : int
+     The cost of the max cut solution.
+ """
+ x_vec = results[0][0]
+ cost = 0
+ for edge in edges:
+     n1, n2 = edge
+     cost += int(x_vec[n1]) * (1-int(x_vec[n2])) + int(x_vec[n2]) * (1-int(x_vec[n1]))
+ return cost
+
+
+def visualize_max_cut(G, partition, title):
+    """
+    Visualize the graph with the max cut highlighted
+
+    Parameters
+    ----------
+    G : networkx.Graph
+        The input graph
+    partition : tuple
+        A partition of the nodes of G into two sets
+    title : str
+        The title of the plot
+
+    Returns
+    -------
+    None
+    """
+    # Prepare to plot the graph
+    pos = nx.spring_layout(G)  # positions for all nodes using the spring layout
+
+    # Draw 
+    nx.draw_networkx_nodes(G, pos, nodelist=partition[0], node_color='red', node_size=500, alpha=0.8) 
+    nx.draw_networkx_nodes(G, pos, nodelist=partition[1], node_color='blue', node_size=500, alpha=0.8)
+    nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
+
+    # Draw node labels
+    nx.draw_networkx_labels(G, pos)
+
+    # Set plot title
+    plt.title(title)
+    plt.axis('off')  # Turn off the axis
+    plt.show()
+    
+
+def partition_to_bin(partition):
+    """
+    This function converts a max cut partition of a graph into a binary number.
+
+    Parameters
+    ----------
+    partition : tuple
+        A tuple containing two sets of nodes that represent the partition of a graph.
+
+    Returns
+    -------
+    binary_string : str
+        A binary number that represents the partition of the graph.
+    """
+    # Define the subsets
+    subset_zero = partition[0]
+    subset_one = partition[1]
+
+    # Determine the maximum index to define the size of the binary number
+    max_index = max(max(subset_zero), max(subset_one))  # Find the highest index
+
+    # Initialize the binary number list with zeros
+    binary_number = [0] * (max_index + 1)
+
+    # Set the positions in the binary number according to the subsets
+    for index in subset_one:
+        binary_number[index] = 1
+
+    # Since the binary number needs to be expressed as an actual number, convert list to string and then to a binary number
+    binary_string = ''.join(str(bit) for bit in binary_number)
+
+    # Print results
+    # print("Max cut solution, classical:", binary_string)
+    return binary_string
+
+
+def bin_to_partition(binary_string):
+    """
+    This function converts a binary number into a max cut partition of a graph.
+
+    Parameters
+    ----------
+    binary_string : str
+        A binary number that represents the partition of the graph.
+
+    Returns
+    -------
+    partition : tuple
+        A tuple containing two sets of nodes that represent the partition of a graph.
+    """
+    # Convert the binary number to a list of integers
+    binary_list = [int(bit) for bit in binary_string]
+
+    # Define the subsets
+    subset_zero = set()
+    subset_one = set()
+
+    # Set the positions in the subsets according to the binary number
+    for index, bit in enumerate(binary_list):
+        if bit == 0:
+            subset_zero.add(index)
+        else:
+            subset_one.add(index)
+
+    # Define the partition
+    partition = (subset_zero, subset_one)
+
+    # Print results
+    # print("Max cut solution, quantum:", partition)
+    return partition
