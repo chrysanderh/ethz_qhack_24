0e9aef980e5784361ca348df48b24fd02ccdf33d
Paul Uriarte Vicandi <pauluv@ni.eus>
2024-05-05 08:16:18 +0000

    Cleanup

--- run_dac.sh
+++ run_dac.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-for i in {12..18..2}; do
+for i in {12..30..2}; do
     python QAOA_square.py results_dac.csv $((i*2)) $i
 done
-python QAOA_square.py results_dac.csv 24 12 &&
-python QAOA_square.py results_dac.csv 28 14 &&
-python QAOA_square.py results_dac.csv 32 16 &&
-python QAOA_square.py results_dac.csv 36 18 &&
-python QAOA_square.py results_dac.csv 40 20 &&
-python QAOA_square.py results_dac.csv 44 15 &&
-python QAOA_square.py results_dac.csv 48 16 &&
-python QAOA_square.py results_dac.csv 52 17 &&
-python QAOA_square.py results_dac.csv 56 19 &&
-python QAOA_square.py results_dac.csv 60 20 &&
-python QAOA_square.py results_dac.csv 64 16 &&
-python QAOA_square.py results_dac.csv 68 17 &&
-python QAOA_square.py results_dac.csv 72 18
